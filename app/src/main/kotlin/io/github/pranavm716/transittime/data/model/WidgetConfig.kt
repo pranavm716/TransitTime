80822d2171ecb4f2d516c3545da48256b37271aa
Pranav Manjapra <pranav.manjapra@gmail.com>
2026-03-22 01:18:04 -0700

    adding widget configuration

---
+++ app/src/main/kotlin/io/github/pranavm716/transittime/data/model/WidgetConfig.kt
@@ -0,0 +1,19 @@
+package io.github.pranavm716.transittime.data.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "widget_configs")
+data class WidgetConfig(
+    @PrimaryKey
+    val widgetId: Int,
+    val stopId: String,
+    val stopName: String,
+    val agency: Agency,
+    val filteredHeadsigns: List<String>,
+    val maxArrivals: Int,
+) {
+    init {
+        require(maxArrivals in 1..5) { "maxArrivals must be between 1 and 5." }
+    }
+}
