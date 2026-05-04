e57c8c6b39f1db7079160d78349761da69c62be6
Pranav Manjapra <pranav.manjapra@gmail.com>
2026-05-03 19:35:13 -0700

    scaffolding watch project structure

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,3 +25,4 @@ dependencyResolutionManagement {
 rootProject.name = "TransitTime"
 include(":app")
 include(":shared")
+include(":wear")
---
+++ wear/src/main/kotlin/io/github/pranavm716/transittime/wear/WearMainActivity.kt
@@ -0,0 +1,10 @@
+package io.github.pranavm716.transittime.wear
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class WearMainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
